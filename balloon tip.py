9b6aea6c4e123714429490d8c6f773213963135d
zhangxuqiang <zhangxuqiang@kkanimation.com>
2017-10-18 11:19:47 +0800

    [Add]:第一次初步完成balloon tip

---
+++ balloon tip.py
@@ -0,0 +1,77 @@
+# coding:utf-8
+
+import PySide.QtGui as QtGui
+import PySide.QtCore as QtCore
+import shiboken
+import maya.OpenMayaUI as apiUI
+
+
+def getMayaWindow():
+    ptr = apiUI.MQtUtil.mainWindow()
+    if ptr is not None:
+        return shiboken.wrapInstance(long(ptr), QtGui.QMainWindow)
+
+
+class popupMessage(QtGui.QLabel):
+    def __init__(self, parent, message="", timeout=2000):
+        super(popupMessage, self).__init__(parent)
+        self.message = message
+        self.setMargin(10)
+        self.timer = QtCore.QTimer()
+        self.timer.start(timeout)
+        self.timer.timeout.connect(self.times_up)  # connect it to your update function
+
+        effect = QtGui.QGraphicsDropShadowEffect()
+        effect.setBlurRadius(10)
+        effect.setOffset(2, 2)
+        self.setGraphicsEffect(effect)
+
+        self.showMessage(self.message)
+
+        self.setStyleSheet('''
+            QLabel {
+                background-color: rgba(80,150,250,200);
+                border: 2px solid rgba(60,130,250,250);
+                border-radius: 4px;
+            }
+        ''')
+
+    def times_up(self):
+        self.close()
+        self.parentWidget().adjustSize()
+        self.parentWidget().update()
+
+    def showMessage(self, message=None):
+        self.setText(message)
+        self.adjustSize()
+        self.update()
+        self.show()
+
+        # center = self.parentWidget().size()
+        # self.move(center.width() * .5 - self.width() * .5, 10);
+
+
+class messageArea(QtGui.QDialog):
+    def __init__(self, parent):
+        super(messageArea, self).__init__(parent)
+        self.setFixedWidth(200)
+        self.mainlayout = QtGui.QVBoxLayout()
+        self.mainlayout.setContentsMargins(0, 0, 0, 0)
+        self.mainlayout.setSpacing(2)
+        self.setLayout(self.mainlayout)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint | QtCore.Qt.WindowStaysOnTopHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        for x in range(5):
+            self.mainlayout.addWidget(popupMessage(self, 'this is meassage({})'.format(x), 2000 + 1000 * x))
+
+        center = self.parentWidget().size()
+        self.move(center.width() * .5 - self.width() * .5, center.height() * .5)
+
+    def showUI(self):
+        self.update()
+        self.adjustSize()
+        self.show()
+
+
+m = messageArea(getMayaWindow())
+m.showUI()
