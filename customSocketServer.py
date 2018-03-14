927183c974bf17857b5a009abeafe445a2cbf371
zhangxuqiang <zhangxuqiang@kkanimation.com>
2018-03-14 14:58:33 +0800

    [Add]:socketServer测试暂时正常

--- customSocketServer.py
+++ customSocketServer.py
@@ -5,8 +5,12 @@ except ImportError:
     import socketserver as ss
 import threading
 import socket
+import json
+import struct
 
 ADDRESS, PORT = "localhost", 10010
+HeaderSize = 4
+HeaderFormat = '!i'
 
 
 def _port_is_free(port):
@@ -21,7 +25,57 @@ def _port_is_free(port):
 
 class ProcessBlock(ss.BaseRequestHandler):
     def handle(self):
-        print('done')
+        lock = threading.Lock()
+        lock.acquire()
+        while True:
+            data = self.request.recv(1024)
+            if data:
+                # -------------------------------------------------------
+                # 把数据存入缓冲区，类似于push数据
+                self.server.data_buffer += data
+                while True:
+                    if len(self.server.data_buffer) < HeaderSize:
+                        print(u"数据包（%s Byte）小于消息头部长度，跳出小循环" % len(self.server.data_buffer))
+                        break
+
+                    # 读取包头
+                    # struct中:!代表Network order，2I代表2个unsigned int数据
+                    headPack = struct.unpack(HeaderFormat, self.server.data_buffer[:HeaderSize])
+                    currentDataPackage_bodySize = headPack[0]
+
+                    # 分包情况处理，跳出函数继续接收数据
+                    if len(self.server.data_buffer) < HeaderSize + currentDataPackage_bodySize:
+                        print(u"数据包（%s Byte）不完整（总共%s Byte）,跳出小循环" % (
+                            len(self.server.data_buffer), HeaderSize + currentDataPackage_bodySize))
+                        break
+                        # 读取消息正文的内容
+                    body = self.server.data_buffer[
+                           HeaderSize:HeaderSize + currentDataPackage_bodySize]
+
+                    # 数据处理
+                    try:
+                        self.unpack_data(headPack, body)
+                    finally:
+                        # 粘包情况的处理
+                        # 获取下一个数据包，把当前处理的相关数据pop出
+                        self.server.data_buffer = self.server.data_buffer[HeaderSize + currentDataPackage_bodySize:]
+                        lock.release()
+                        return
+                        # -------------------------------------------------------
+
+    def unpack_data(self, header, body):
+        # header在这一步以前已经被unpack了
+        body_raw = json.loads(body.decode())
+        self.process_data(header, body_raw)
+
+    def process_data(self, header, body):
+        pass
+
+
+class ProcessServer(ss.ThreadingMixIn, ss.TCPServer):
+    def __init__(self, (HOST, PORT), requestHandle):
+        ss.TCPServer.__init__(self, (HOST, PORT), requestHandle)
+        self.data_buffer = bytes()
 
 
 class ResponseServer(object):
@@ -32,17 +86,20 @@ class ResponseServer(object):
         self._socketServerHandler = None
 
     def instanll_process_block(self, block):
-        if block and issubclass(block, ss.BaseRequestHandler):
+        if block and issubclass(block, ProcessBlock):
             self._socketServerHandler = block
+            print('register success.')
 
     def start(self):
         if not self.status and self._socketServerHandler:
             # 未运行
-            self._socketServer = ss.ThreadingTCPServer((ADDRESS, PORT), self._socketServerHandler)
+            self._socketServer = ProcessServer((ADDRESS, PORT), self._socketServerHandler)
             self._thread = threading.Thread(target=self._socketServer.serve_forever)
             self._thread.daemon = True
             self._thread.start()
             self.status = True
+        else:
+            raise Exception('Can not start server.')
 
     def shut_Down(self):
         if self.status:
@@ -50,4 +107,5 @@ class ResponseServer(object):
             self._socketServer.server_close()
             self._thread = None
             self.status = False
-
+        else:
+            print('server already die.')
